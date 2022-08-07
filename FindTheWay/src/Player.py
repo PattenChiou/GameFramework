7f2b07997a33744c6672af1883baf40df0044f2f
PattenChiou <patten1719@gmail.com>
2022-08-07 10:34:50 +0800

    rotate,move according to the way it towards

--- FindTheWay/src/Player.py
+++ FindTheWay/src/Player.py
@@ -6,6 +6,7 @@ from mlgame.utils.enum import StringEnum
 from mlgame.view.view_model import create_asset_init_data, create_image_view_data
 
 import env
+import math
 
 PLAYER_PATH = path.join(path.dirname(__file__), "..", "asset", "image", "player.png")
 BULLET_PATH=path.join(path.dirname(__file__),"..","asset","image","treasure.png")
@@ -15,23 +16,42 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos: tuple, size: tuple, play_area_rect: pygame.Rect):
         super().__init__()
         self._play_area_rect = play_area_rect
-        self._speed = 5
+        self._speed = 4
         self._init_pos = pos
         self.rect = pygame.Rect(*pos, *size)
         self._score = 0
         self.bullet=[Bullet((-2.5,-2.5),(5,5))]
+        self._angle=0
 
     def update(self, action) -> None:
         print(action)
+        while(self._angle<0):
+            self._angle+=360
         for i in range(0,len(action)):
             if action[i] == "UP" and self.rect.top > self._play_area_rect.top:
-                self.rect.centery -= self._speed
+                if(self._angle%360==0):
+                        self.rect.centery-=self._speed
+                elif(self._angle%360==90):
+                    self.rect.centerx -= self._speed
+                elif(self._angle%360==180):
+                        self.rect.centery+=self._speed
+                elif (self._angle%360==270):
+                        self.rect.centerx+=self._speed
             elif action[i] == "DOWN" and self.rect.bottom < self._play_area_rect.bottom:
-                self.rect.centery += self._speed
+                if(self._angle%360==0):
+                        self.rect.centery+=self._speed
+                elif(self._angle%360==90):
+                    self.rect.centerx += self._speed
+                elif(self._angle%360==180):
+                        self.rect.centery-=self._speed
+                elif (self._angle%360==270):
+                        self.rect.centerx-=self._speed
             elif action[i] == "LEFT" and self.rect.left > self._play_area_rect.left:
-                self.rect.centerx -= self._speed
+                #self.rect.centerx -= self._speed
+                self._angle+=90
             elif action[i] == "RIGHT" and self.rect.right < self._play_area_rect.right:
-                self.rect.centerx += self._speed
+                #self.rect.centerx += self._speed
+                self._angle-=90
             elif action[i]=="F":
                 self.bullet.append(Bullet((self.rect.centerx,self.rect.centery),(5,5)))
             elif action[i]=="SHOOT":
@@ -65,7 +85,7 @@ class Player(pygame.sprite.Sprite):
     @property
     def game_object_data(self):
         return create_image_view_data(image_id="player", x=self.rect.x, y=self.rect.y,
-                                      width=self.rect.width, height=self.rect.height, angle=0)
+                                      width=self.rect.width, height=self.rect.height, angle=self._angle*math.pi/180)
 
     @property
     def game_init_object_data(self):
